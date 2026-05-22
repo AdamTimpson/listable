7d3feb7f7b2be69e194b8489060a91a6dd84b3fd
Adam Timpson <adamtimpson@gmail.com>
2026-05-22 17:50:11 +0100

    A python CLI but make it a Java GUI

---
+++ src/App.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        JFrame frame = new JFrame();
+        frame.setSize(new Dimension(600, 400));
+        frame.setTitle("Title");
+        frame.setVisible(true);
+    }
+}
